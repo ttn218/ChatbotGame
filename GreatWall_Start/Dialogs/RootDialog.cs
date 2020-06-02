e25fc032c1e5bcf9dca3482bc2fbd3d50d861256
ttn218 <54901871+ttn218@users.noreply.github.com>
2020-06-02 15:15:44 +0900

    test

---
+++ GreatWall_Start/Dialogs/RootDialog.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Net.Http;
+using GreatWall.Dialogs;
+using System.Collections.Generic;
+
+namespace GreatWall
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        protected int count = 1;
+        string strMessage;
+        private string strWelcomMMessage = "[Great Wall Bot]";
+
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            await context.PostAsync(strWelcomMMessage);
+
+            var message = context.MakeMessage();
+            var actions = new List<CardAction>();
+
+            actions.Add(new CardAction() { Title = "1. Order", Value="1", Type=ActionTypes.ImBack });
+            actions.Add(new CardAction() { Title = "2. FAQ", Value = "2", Type = ActionTypes.ImBack });
+
+            message.Attachments.Add(new HeroCard { Title = "Select 1 or 2.>", Buttons = actions }.ToAttachment());
+
+            await context.PostAsync(message);
+
+            context.Wait(SendWelcomeMessageAsync);
+        }
+
+        public async Task SendWelcomeMessageAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            Activity activity = await result as Activity;
+            string strSelected = activity.Text.Trim();
+
+            if(strSelected == "1")
+            {
+                context.Call(new OrderDialog(), DialogResumeAfter);
+            }
+            else if(strSelected == "2")
+            {
+                strMessage = "[FAQ Service] Please enter a question.";
+                await context.PostAsync(strMessage);
+
+                context.Call(new FAQDialog(), DialogResumeAfter);
+            }
+            else
+            {
+                strMessage = "You have made a mistake. Please select again...";
+                await context.PostAsync(strMessage);
+                context.Wait(SendWelcomeMessageAsync);
+            }
+        }
+
+        public async Task DialogResumeAfter(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                strMessage = await result;
+                await this.MessageReceivedAsync(context, result);
+            }
+            catch(TooManyAttemptsException)
+            {
+                await context.PostAsync("Error occurred...");
+            }
+        }
+
+        public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        {
+            var confirm = await argument;
+            if (confirm)
+            {
+                this.count = 1;
+                await context.PostAsync("Reset count.");
+            }
+            else
+            {
+                await context.PostAsync("Did not reset count.");
+            }
+            context.Wait(MessageReceivedAsync);
+        }
+
+    }
+}
---
+++ GreatWall_Start/Helpers/SQLHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GreatWall.Helpers
+{
+    public class SQLHelper
+    {
+        private const string strDBServer = "Server=tcp:greatwall-db-server.database.windows.net,1433;" +
+                               "Initial Catalog=GreatWallDB;Persist Security Info=False;" +
+                                "User ID=non778;Password=asdf1245*;" +
+                                "MultipleActiveResultSets=False;" +
+                                "Encrypt=True;" +
+                                "TrustServerCertificate=False;" +
+                                "Connection Timeout=30;";
+
+        public static DataSet RunSQL(string strQuery)
+        {
+            SqlConnection DB_CON = new SqlConnection(strDBServer);
+            SqlCommand DB_Query = new SqlCommand(strQuery, DB_CON);
+            SqlDataAdapter DB_Adapter = new SqlDataAdapter(DB_Query);
+
+            DataSet DB_DS = new DataSet();
+            DB_Adapter.Fill(DB_DS);
+
+            return DB_DS;
+        }
+
+        public static void ExecuteNonQuery(string strQuery, SqlParameter[] para)
+        {
+            SqlConnection DB_CON = new SqlConnection(strDBServer);
+            SqlCommand DB_Query = new SqlCommand(strQuery, DB_CON);
+            DataSet DB_DS = new DataSet();
+
+            DB_Query.Parameters.Clear();
+
+            foreach(SqlParameter p in para)
+            {
+                DB_Query.Parameters.Add(p);
+            }
+
+            DB_CON.Open();
+            DB_Query.ExecuteNonQuery();
+            DB_CON.Close();
+        }
+    }
+}
