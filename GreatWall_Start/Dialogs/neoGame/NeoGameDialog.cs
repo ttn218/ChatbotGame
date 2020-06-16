0ec91399ff0eaaad01130035b63df1528803df23
jseoup <crobat126@naver.com>
2020-06-16 19:33:17 +0900

    게임 설명 추가
    게임 설명 추가

--- GreatWall_Start/Dialogs/neoGame/NeoGameDialog.cs
+++ GreatWall_Start/Dialogs/neoGame/NeoGameDialog.cs
@@ -13,6 +13,13 @@ namespace LionKing.Dialogs.neoGame
     public class NeoGameDialog : IDialog<string>
     {
         string type = "neo";
+        string manual = "2. neoGame (신조어 게임)" + Environment.NewLine
+                        + "아재왕! 게임은 제시되는 설명을 읽고, 해당 뜻이 의미하는 신조어를 맞추는 게임입니다." + Environment.NewLine + Environment.NewLine
+                        + "제한 시간은 300초이며, 시간 내에 가능한 한 많은 문제를 맞혀 높은 점수를 따내면 되는 게임입니다." + Environment.NewLine
+
+                        + "난이도는 [Normal], [Hard] 두 가지로 구성됩니다." + Environment.NewLine
+                        + "[Normal] 난이도는 3지 선다의 객관식 문제로 출제됩니다." + Environment.NewLine
+                        + "[Hard] 난이도는 주관식 문제로 출제됩니다.";
 
         public async Task StartAsync(IDialogContext context)
         {
@@ -39,6 +46,11 @@ namespace LionKing.Dialogs.neoGame
             if (strSelected == "게임시작")
             {
                 context.Call(new GameStartDialog(), DialogResumeAfter);
+            }
+            else if (strSelected == "게임설명")
+            {
+                await context.PostAsync(manual);
+                await StartAsync(context);
             }
             else if (strSelected == "랭킹")
             {
